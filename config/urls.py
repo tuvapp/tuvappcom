5ca2be19f4b0c86a1d4a9553711a4da9d3f32841
emilingemarkarlsson <emilkarlsson@Emils-MacBook-Air.local>
2016-08-29 20:04:16 +0200

    upload

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,11 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+        url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^product/$', TemplateView.as_view(template_name='pages/product.html'), name='product'),
+    url(r'^contact/$', TemplateView.as_view(template_name='pages/contact.html'), name='contact'),
+    url(r'^press/$', TemplateView.as_view(template_name='pages/press.html'), name='press'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, include(admin.site.urls)),
